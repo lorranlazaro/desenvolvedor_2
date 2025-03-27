c6084243f1c18862170a30978b900a0c4e331d4b
Lorran Lazaro <lorranlazaro@gmail.com>
2025-03-27 20:50:56 -0300

    desafios 1 e 2.

---
+++ tecnica_n1.dart
@@ -0,0 +1,17 @@
+void main() {
+  /* 1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA? */
+
+  int indice = 13;
+  int soma = 0;
+  int k = 0;
+
+  while (k < indice) {
+    k = k + 1;
+    soma = soma + k;
+  }
+
+  print(soma);
+}
